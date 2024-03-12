4bfbc0baf3a16da8cef56d8de1d00e9bb6539ea0
Elizabeth Gorbunova <egorbunova016@gmail.com>
2024-03-12 20:48:31 +0100

    ConsoleApp with implemented fixes

--- ConsoleApp/DataReader.cs
+++ ConsoleApp/DataReader.cs
@@ -13,7 +13,10 @@
 
         public void ImportAndPrintData(string fileToImport, bool printData = true)
         {
-            ImportedObjects = new List<ImportedObject>() { new ImportedObject() };
+            ImportedObjects = new List<ImportedObject>() 
+            { 
+                new ImportedObject() 
+            };
 
             var streamReader = new StreamReader(fileToImport);
 
@@ -21,32 +24,58 @@
             while (!streamReader.EndOfStream)
             {
                 var line = streamReader.ReadLine();
+                if (!(line == String.Empty))                                                                             //2 Added check if line is empty
                 importedLines.Add(line);
             }
+            /*foreach(var line in importedLines)
+            {
+                Console.WriteLine(line);
+            }*/
 
-            for (int i = 0; i <= importedLines.Count; i++)
+            for (int i = 1; i < importedLines.Count; i++)                                                            //3 Correction from 0 to 1(avoid headers)   and -1 added when count lines
             {
                 var importedLine = importedLines[i];
                 var values = importedLine.Split(';');
                 var importedObject = new ImportedObject();
-                importedObject.Type = values[0];
-                importedObject.Name = values[1];
-                importedObject.Schema = values[2];
-                importedObject.ParentName = values[3];
-                importedObject.ParentType = values[4];
-                importedObject.DataType = values[5];
-                importedObject.IsNullable = values[6];
+                
+                try                                                                                                      //4 Try catch implemented
+                   {
+                    importedObject.Type = values[0];
+                    importedObject.Name = values[1];
+                    importedObject.Schema = values[2];
+                    importedObject.ParentName = values[3];
+                    importedObject.ParentType = values[4];
+                    importedObject.DataType = values[5];
+                    importedObject.IsNullable = values[6]; 
+                   }
+                catch
+                {
+                    new ApplicationException("Error while assigning values from a table");
+                }
+
+
                 ((List<ImportedObject>)ImportedObjects).Add(importedObject);
             }
 
             // clear and correct imported data
             foreach (var importedObject in ImportedObjects)
             {
-                importedObject.Type = importedObject.Type.Trim().Replace(" ", "").Replace(Environment.NewLine, "").ToUpper();
-                importedObject.Name = importedObject.Name.Trim().Replace(" ", "").Replace(Environment.NewLine, "");
-                importedObject.Schema = importedObject.Schema.Trim().Replace(" ", "").Replace(Environment.NewLine, "");
-                importedObject.ParentName = importedObject.ParentName.Trim().Replace(" ", "").Replace(Environment.NewLine, "");
-                importedObject.ParentType = importedObject.ParentType.Trim().Replace(" ", "").Replace(Environment.NewLine, "");
+                try                                                                                                                  //5 Try catch added
+                {
+                    if (!(importedObject.DataType == null))                                                                          //6 Check if null added
+                    {
+                        importedObject.Type = importedObject.Type.Trim().Replace(" ", "").Replace(Environment.NewLine, "").ToUpper();
+                        importedObject.Name = importedObject.Name.Trim().Replace(" ", "").Replace(Environment.NewLine, "");
+                        importedObject.Schema = importedObject.Schema.Trim().Replace(" ", "").Replace(Environment.NewLine, "");
+                        importedObject.ParentName = importedObject.ParentName.Trim().Replace(" ", "").Replace(Environment.NewLine, "");
+                        importedObject.ParentType = importedObject.ParentType.Trim().Replace(" ", "").Replace(Environment.NewLine, "");
+                    }
+                    
+                }
+                catch
+                {
+                    new ApplicationException("Error while correcting data");
+                }
             }
 
             // assign number of children
@@ -59,7 +88,7 @@
                     {
                         if (impObj.ParentName == importedObject.Name)
                         {
-                            importedObject.NumberOfChildren = 1 + importedObject.NumberOfChildren;
+                            importedObject.NumberOfChildren ++;                                                                      //7 Increment changed
                         }
                     }
                 }
@@ -67,31 +96,38 @@
 
             foreach (var database in ImportedObjects)
             {
-                if (database.Type == "DATABASE")
+                if (database.Type == "DATABASE" )                                                  
                 {
                     Console.WriteLine($"Database '{database.Name}' ({database.NumberOfChildren} tables)");
 
                     // print all database's tables
                     foreach (var table in ImportedObjects)
                     {
-                        if (table.ParentType.ToUpper() == database.Type)
+                        try
                         {
-                            if (table.ParentName == database.Name)
+                            if (!(table.ParentType==null) && table.ParentType.ToUpper() == database.Type)                               //8 Check if null added
                             {
-                                Console.WriteLine($"\tTable '{table.Schema}.{table.Name}' ({table.NumberOfChildren} columns)");
-
-                                // print all table's columns
-                                foreach (var column in ImportedObjects)
+                                if (table.ParentName == database.Name)
                                 {
-                                    if (column.ParentType.ToUpper() == table.Type)
+                                    Console.WriteLine($"\tTable '{table.Schema}.{table.Name}' ({table.NumberOfChildren} columns)");
+
+                                    // print all table's columns
+                                    foreach (var column in ImportedObjects)
                                     {
-                                        if (column.ParentName == table.Name)
+                                        if (!(column.ParentType==null) && column.ParentType.ToUpper() == table.Type)                   //9 Check if null added
                                         {
-                                            Console.WriteLine($"\t\tColumn '{column.Name}' with {column.DataType} data type {(column.IsNullable == "1" ? "accepts nulls" : "with no nulls")}");
+                                            if (column.ParentName == table.Name)
+                                            {
+                                                Console.WriteLine($"\t\tColumn '{column.Name}' with {column.DataType} data type {(column.IsNullable == "1" ? "accepts nulls" : "with no nulls")}");
+                                            }
                                         }
                                     }
                                 }
                             }
+                        }
+                        catch(Exception ex)
+                        {
+                            Console.WriteLine(ex.ToString());
                         }
                     }
                 }
